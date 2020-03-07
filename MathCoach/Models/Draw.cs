3d063290487816ffc9cc506d6a9e8faece6625f2
lzlocki <lzlocki@poczta.fm>
2020-03-07 19:45:12 +0100

    Adding Draw Model and UI

---
+++ MathCoach/Models/Draw.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCoach.Models
+{
+    class Draw
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        private int boundary = 10;
+
+        //constr rtyrty
+        public Draw()
+        {
+            // random ewrwerewr
+            Random random = new Random();
+            FirstNumber = random.Next(0, boundary);
+            SecondNumber = random.Next(0, boundary);
+        }
+
+    }
+}
