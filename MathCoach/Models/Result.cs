d100eaa7bb3f17c7897bcaf976284de40c2c3a50
lzlocki <lzlocki@poczta.fm>
2020-03-14 19:50:54 +0100

    add Result class

---
+++ MathCoach/Models/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCoach.Models
+{
+    class Result
+    {
+        public int OK { get; set; }
+        public int NOK { get; set; }
+        List<Draw> DrawList = new List<Draw>();
+
+        public void AddDrawResult(Draw draw)
+        {
+            DrawList.Add(draw);
+        }
+
+
+    }
+}
